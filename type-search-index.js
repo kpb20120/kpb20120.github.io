ad0485eca2f06b420ac7683502cdce33966e97a7
Dominic <dominic.cantwell.2020@uni.strath.ac.uk>
2023-03-09 19:17:43 +0000

    Added files to src package and cleaned up some code

--- type-search-index.js
+++ type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Game"},{"p":"<Unnamed>","l":"GameInterface"},{"p":"<Unnamed>","l":"GraphADT"},{"p":"<Unnamed>","l":"GraphInterface"},{"p":"<Unnamed>","l":"GUI"},{"p":"<Unnamed>","l":"Player"},{"p":"<Unnamed>","l":"PlayerInterface"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"src","l":"Game"},{"p":"src","l":"GameInterface"},{"p":"src","l":"GraphADT"},{"p":"src","l":"GraphInterface"},{"p":"src","l":"GUI"},{"p":"src","l":"Player"},{"p":"src","l":"PlayerInterface"}]
